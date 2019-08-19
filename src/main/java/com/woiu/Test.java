a770674828cd0bdd7acb55e800e6311ab814d94b
hyj <1025886312@qq.com>
2019-08-19 16:27:15 +0800

    111

--- src/main/java/com/woiu/Test.java
+++ src/main/java/com/woiu/Test.java
@@ -4,8 +4,7 @@ import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.util.Factory;
 
-public class Test {11111111
-
+public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
