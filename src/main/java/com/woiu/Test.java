a99c3e12e6f98549f39e56d88173f2bd6abbd5a8
hyj <1025886312@qq.com>
2019-08-19 15:40:41 +0800

    rqewr

---
+++ src/main/java/com/woiu/Test.java
@@ -0,0 +1,16 @@
+package com.woiu;
+
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.util.Factory;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+		SecurityManager securityManager = factory.getInstance();
+
+	}
+
+}
